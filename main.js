1e6208921d2d616f51d49f197c7c02a2df56762e
ErolledDev <107593932+ErolledDev@users.noreply.github.com>
2023-04-01 12:14:57 +0800

    v1

---
+++ main.js
@@ -0,0 +1,19 @@
+function merge() {
+  const urlInput = document.getElementById("urlInput").value.trim();
+  const messageInput = document.getElementById("messageInput").value.trim();
+
+  // Validate URL
+  const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+  if (!urlPattern.test(urlInput)) {
+    document.getElementById("url-error").textContent = "Please enter a valid URL.";
+    document.getElementById("mergeUrl").value = "";
+    return;
+  } else {
+    document.getElementById("url-error").textContent = "";
+  }
+
+  const encodedMessage = encodeURIComponent(messageInput);
+  const mergedUrl = `https://www.mywebsite.com/redirect.html?url=${encodeURIComponent(urlInput)}&message=${encodedMessage}`;
+
+  document.getElementById("mergeUrl").value = mergedUrl;
+}
